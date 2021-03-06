3926bba2193c371ebbd4f262e8fe40556175f707
Iryna Konoplova <a.irina773@gmail.com>
2021-03-06 02:03:23 +0200

    Решает задачи 1 и 2

---
+++ js/task-3.js
@@ -0,0 +1,87 @@
+// Напиши функцию findBestEmployee(employees), которая принимает объект
+// сотрудников и возвращает имя самого продуктивного(который выполнил больше всех задач).
+// Сотрудники и кол - во выполненых задач содержатся как свойства объекта в формате
+// "имя": "кол-во задач".
+
+// const findBestEmployee = function(employees) {
+//   // твой код
+// };
+
+// /*
+//  * Вызовы функции для проверки работоспособности твоей реализации.
+//  */
+// console.log(
+//   findBestEmployee({
+//     ann: 29,
+//     david: 35,
+//     helen: 1,
+//     lorence: 99,
+//   }),
+// ); // lorence
+
+// console.log(
+//   findBestEmployee({
+//     poly: 12,
+//     mango: 17,
+//     ajax: 4,
+//   }),
+// ); // mango
+
+// console.log(
+//   findBestEmployee({
+//     lux: 147,
+//     david: 21,
+//     kiwi: 19,
+//     chelsy: 38,
+//   }),
+// ); // lux
+
+
+const findBestEmployee = function(employees) {
+    
+    const arrayEmployee = Object.keys(employees);
+    console.log(arrayEmployee);
+    
+    
+
+    let bestEmployee = 0;
+    // console.log(bestEmployee);
+
+    for (const arr of arrayEmployee) {
+        if (bestEmployee < employees[arr]) {
+            bestEmployee = employees[arr];
+       }
+        // console.log(employees[arr]);
+
+    }
+    return bestEmployee;
+ };
+
+
+
+
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  }),
+); // lorence
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  }),
+); // mango
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }),
+); // lux
